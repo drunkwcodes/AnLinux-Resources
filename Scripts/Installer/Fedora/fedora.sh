5f4c8ef0ad579a42e2fd417026c7a49bd3cca222
drunkwcodes <36228443+drunkwcodes@users.noreply.github.com>
2019-04-14 09:56:38 +0800

    Update fedora.sh

--- Scripts/Installer/Fedora/fedora.sh
+++ Scripts/Installer/Fedora/fedora.sh
@@ -77,6 +77,4 @@ echo "fixing shebang of $bin"
 termux-fix-shebang $bin
 echo "making $bin executable"
 chmod +x $bin
-echo "removing image for some space"
-rm $tarball
 echo "You can now launch Fedora with the ./${bin} script"
